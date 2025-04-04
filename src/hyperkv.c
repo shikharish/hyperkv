9ed3324b0d242c089c2e7e383ff75625d8cd42e5
Shikhar Soni <shikharish05@gmail.com>
2025-04-04 09:54:22 +0530

    init. basic server implementation
    Signed-off-by: Shikhar Soni <shikharish05@gmail.com>

---
+++ src/hyperkv.c
@@ -0,0 +1,42 @@
+#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static void print_intro() {
+	const char *intro = 
+		" █████   █████                                         █████   ████ █████   █████\n"
+		"░░███   ░░███                                         ░░███   ███░ ░░███   ░░███ \n"
+		" ░███    ░███  █████ ████ ████████   ██████  ████████  ░███  ███    ░███    ░███ \n"
+		" ░███████████ ░░███ ░███ ░░███░░███ ███░░███░░███░░███ ░███████     ░███    ░███ \n"
+		" ░███░░░░░███  ░███ ░███  ░███ ░███░███████  ░███ ░░░  ░███░░███    ░░███   ███  \n"
+		" ░███    ░███  ░███ ░███  ░███ ░███░███░░░   ░███      ░███ ░░███    ░░░█████░   \n"
+		" █████   █████ ░░███████  ░███████ ░░██████  █████     █████ ░░████    ░░███     \n"
+		"░░░░░   ░░░░░   ░░░░░███  ░███░░░   ░░░░░░  ░░░░░     ░░░░░   ░░░░      ░░░      \n"
+		"                ███ ░███  ░███                                                   \n"
+		"               ░░██████   █████                                                  \n"
+		"                ░░░░░░   ░░░░░                                                   \n";
+
+	printf("\n");
+	printf("%s", intro);
+	printf("\n");
+	printf("       Developed by: Shikhar Soni\n");
+	printf("\n");
+}
+
+static void close_server(int sfd, HashTable *ht) {
+	close_socket(sfd);
+	htable_free(ht);
+	exit(0);
+}
+
+int main() {
+	HashTable *ht = htable_init(HT_BASE_SIZE);
+	print_intro();
+	int sfd = init_server();
+	while (1) {
+		int cfd = accept_connection(sfd);
+		int code = rediskw(cfd, ht);
+		code == 1 ? close_server(sfd, ht) : close_socket(cfd);
+	}
+	return 0;
+}
