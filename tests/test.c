2d4fbde58b4dc88a825394be2217f122e8ccd378
Shikhar Soni <shikharish05@gmail.com>
2025-04-04 09:59:08 +0530

    add tests
    Signed-off-by: Shikhar Soni <shikharish05@gmail.com>

---
+++ tests/test.c
@@ -0,0 +1,19 @@
+#include "test.h"
+#include <stdio.h>
+#include <time.h>
+#define MINIUNIT_MAIN
+#include "miniunit.h"
+#define B 1000000000.0
+
+int main() {
+	double dur;
+	struct timespec start, end;
+	clock_gettime(CLOCK_REALTIME, &start);
+	test_htable();
+	test_parser();
+	test_interpret();
+	clock_gettime(CLOCK_REALTIME, &end);
+	dur = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / B;
+	printf("test duration = %lf\n", dur);
+	return 0;
+}
